0df7b73b29b834399d7aa03d75c8371290323b59
junghee111111 <jungheejjang1004@gmail.com>
2025-06-19 00:46:05 +0900

    fix build errors

--- Source/Assslash/UI/AssslashHUD.cpp
+++ Source/Assslash/UI/AssslashHUD.cpp
@@ -1,5 +1,4 @@
-﻿#include "C:\Users\user\Documents\Unreal Projects\Assslash\Intermediate\Build\Win64\x64\AssslashEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
-#include "AssslashHUD.h"
+﻿#include "AssslashHUD.h"
 
 #include "Components/ProgressBar.h"
 
