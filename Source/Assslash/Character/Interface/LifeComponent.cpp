61540ca5014bea0013e3dadcccbaccd1e374e47e
junghee111111 <jungheejjang1004@gmail.com>
2025-07-07 00:15:32 +0900

    wip: impl. Life Actor Component

---
+++ Source/Assslash/Character/Interface/LifeComponent.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LifeComponent.h"
+
+
+// Sets default values for this component's properties
+ULifeComponent::ULifeComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	HpMax = 100;
+	Hp = HpMax;
+
+	// ...
+}
+
+
+// Called when the game starts
+void ULifeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+// void ULifeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+// {
+// 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+//
+// 	// ...
+// }
+//
