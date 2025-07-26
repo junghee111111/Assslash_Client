82fc4e29bb248034c0c72bcb64cfb98d54c7f718
junghee111111 <jungheejjang1004@gmail.com>
2025-07-26 21:49:40 +0900

    wip: impl. login form

---
+++ Source/Assslash/UI/LoginForm/WidgetLoginForm.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WidgetLoginForm.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSSLASH_API UWidgetLoginForm : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	// ========== Widgets ==========
+	
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	class UEditableTextBox* TextBox_Username;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	class UEditableTextBox* TextBox_Password;
+
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	class UButton* Button_Submit;
+};
